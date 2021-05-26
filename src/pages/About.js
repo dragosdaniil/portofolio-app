31b7fab5fb70395fa0ef8d71f05d38014dea56d3
Dragos <dragossandica997@gmail.com>
2021-05-27 00:19:48 +0300

    created contact and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,38 @@
 import React from "react";
 
+const hero = require("../images/hero.svg");
+
 const About = () => {
   return (
     <main className="page">
-      <h2>About page</h2>
+      <section className="about-page">
+        <div className="about-center section-center">
+          <img src={hero.default} alt="logo" className="hero-svg-about" />
+          <article className="about-me">
+            <header>
+              <h1 className="title">about me</h1>
+              <div className="underline1"></div>
+            </header>
+            <p className="info">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              explicabo, quis deserunt recusandae laborum nemo distinctio ut
+              aspernatur deleniti incidunt necessitatibus corrupti totam placeat
+              nam ad. Cupiditate unde est similique! Facilis voluptatem natus
+              minima doloremque eveniet. Officia optio nam officiis excepturi
+              dolores repellendus! Quo laudantium, modi voluptate earum ducimus
+              sit vel ipsa eum maxime nostrum? Cum at fuga debitis pariatur
+              voluptates facere iste accusantium atque eos. Repudiandae illum
+              error magnam minima ratione, sint delectus adipisci iusto
+              necessitatibus corporis earum qui molestiae provident ab tenetur
+              enim et mollitia dolor dolores, nemo quaerat. Modi, quam dolor
+              vero dignissimos quaerat culpa dolorem. Consequatur?
+            </p>
+            <ul className="tag-list">
+              <li className="tag"></li>
+            </ul>
+          </article>
+        </div>
+      </section>
     </main>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,45 @@
 import React from "react";
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main className="page">
-      <h2>Contact page</h2>
+      <section className="contact-page">
+        <form action="" className="contact-form" onSubmit={handleSubmit}>
+          <div className="form-container">
+            <h2>get in touch</h2>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="form-input"
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-input"
+              placeholder="Email"
+            />
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+              className="message"
+              placeholder="Message"
+            ></textarea>
+          </div>
+
+          <button className="btn form-btn" type="submit">
+            submit here
+          </button>
+        </form>
+      </section>
     </main>
   );
 };
